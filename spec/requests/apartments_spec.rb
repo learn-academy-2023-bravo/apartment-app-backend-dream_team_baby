01301f519dc4919baf64291295d52b4d03365b1a
Valeria <valeriacasillas123@yahoo.com>
2023-05-09 14:57:13 -0700

    CRUD functionality & test complete

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -5,8 +5,7 @@ require 'rails_helper'
 RSpec.describe "Apartments", type: :request do
   user1 = User.where(email: "testing1@example.com").first_or_create(password: "password", password_confirmation: "password")
   describe "GET /index" do
-    # user1 = User.where(email: "testing1@example.com").first_or_create(password: "password", password_confirmation: "password")
-
+    
    it "shows all of the apartments" do
     Apartment.create( 
     street: '45 Rimrock',
@@ -56,8 +55,65 @@ describe "POST /create" do
    expect(apartment.square_footage).to eq 1000
    expect(apartment.city).to eq 'Dallas'
   end
+  describe "DELETE /destroy" do
+    
+    it "it deletes an apartment" do
+     apt1 = Apartment.create( 
+     street: '45 Rimrock',
+     unit: '90A',
+     city: 'Dallas',
+     state: 'TX',
+     square_footage: 1000,
+     price: '1800',
+     bedrooms: 2,
+     bathrooms: 2,
+     pets: 'yes!',
+     image: 'https://images.unsplash.com/photo-1469022563428-aa04fef9f5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80',
+     user_id:user1.id
+     )
+     delete "/apartments/#{apt1.id}"
+     expect(response).to have_http_status(200)
+     expect(Apartment.count).to eq (0)
+    end
+  end
+  describe "PATCH/update" do
+    it "updates an apartment" do
+     apt1= Apartment.create(
+     street: '45 Rimrock',
+     unit: '90A',
+     city: 'San Diego',
+     state: 'TX',
+     square_footage: 1000,
+     price: '1800',
+     bedrooms: 2,
+     bathrooms: 2,
+     pets: 'yes!',
+     image: 'https://images.unsplash.com/photo-1469022563428-aa04fef9f5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80',
+     user_id:user1.id
+     )
+     apartment_params = {
+     apartment: {
+     street: '45 Rimrock',
+     unit: '90A',
+     city: 'San Diego',
+     state: 'TX',
+     square_footage: 1000,
+     price: '1800',
+     bedrooms: 2,
+     bathrooms: 2,
+     pets: 'yes!',
+     image: 'https://images.unsplash.com/photo-1469022563428-aa04fef9f5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80',
+     user_id:user1.id
+      }
+     }
+     patch "/apartments/#{apt1.id}", params: apartment_params
+     expect(response).to have_http_status(200)
+     expect(apt1.street).to eq '45 Rimrock'
+     expect(apt1.bedrooms).to eq 2
+     expect(apt1.square_footage).to eq 1000
+     expect(apt1.city).to eq 'San Diego'
+    end
+  end
 
-
-   
  end
 end
