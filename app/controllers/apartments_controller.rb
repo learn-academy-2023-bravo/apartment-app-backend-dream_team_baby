80bded5cf5ba04e77d23fcb4a02e47a6ea2a9b7e
EddieCarrion.github.io <ecar25L@gmail.com>
2023-05-09 11:54:57 -0700

    validations completed

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,2 +1,27 @@
 class ApartmentsController < ApplicationController
+
+    def index
+        apartments = Apartment.all 
+        render json: apartments
+    end
+    def show
+        apartment = Apartment.find(params[:id])
+        if apartment.valid?
+            render json: apartment
+        else 
+            render json: apartment.errors, status: 422
+        end
+    end
+    def create
+        apartment = Apartment.create(apartment_params)
+        if apartment.valid?
+            render json: apartment
+        else 
+            render json: apartment.errors, status: 422
+        end
+    end
+    private
+    def apartment_params
+        params.require(:apartment).permit(:street, :unit, :city, :state, :square_footage, :pets, :price, :bathrooms, :bedrooms, :image)
+    end
 end
--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,2 +1,6 @@
 class Apartment < ApplicationRecord
+belongs_to :user
+
+validates :street, :unit, :city, :state, :square_footage, :pets, :image, :bathrooms, :bedrooms, :price, presence: true
+
 end
